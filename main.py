a527765537bbc9e50b4f6cf64d2813421d650d92
dkdkdkdkdk1111 <qkdhqkdh0709@gmail.com>
2024-11-08 17:30:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ def main():
     # 페이지 설정 (Streamlit 상단 바 구성)
     st.set_page_config(page_title="김태형", page_icon="💕")  # 웹 페이지 제목과 아이콘 설정
     st.image('body.png')  # 상단에 이미지를 표시
-    st.title("_:green[우리 몸의 구조와 기능]_ 🏫")  # 제목 표시 (우리 몸의 구조와 기능)
-    st.header("😶주의! 이 챗봇은 참고용으로 사용하세요!", divider='blue')  # 주의사항 표시
+    st.title("_:green[우리 몸의 구조와 기능]_ 👀")  # 제목 표시 (우리 몸의 구조와 기능)
+    st.header("😒관련없는 내용 검색 금지👍", divider='blue')  # 주의사항 표시
 
     # 세션 상태 초기화
     # Streamlit 세션에서 대화 상태, 대화 기록, 처리 완료 여부 등을 초기화하여 유지
